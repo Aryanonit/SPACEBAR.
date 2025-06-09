3c1671855dd6d42b140179870304ffe56ca5e3f5
Aryanonit <singharyan4595@gmail.com>
2025-06-09 22:27:40 +0530

    fifth

--- components/particle-background.tsx
+++ components/particle-background.tsx
@@ -108,11 +108,11 @@ export default function ParticleBackground() {
         smooth: true,
         style: {
           position: "fixed",
-          top: 0,
-          left: 0,
+          top: "0",
+          left: "0",
           width: "100%",
           height: "100%",
-          zIndex: -1,
+          zIndex: "-1",
           pointerEvents: "none",
         },
       }}
